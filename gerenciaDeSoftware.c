55f28a6b7a5794a895f30120bfdfb90b2963178d
Stella Marques <stellacompufla@gmail.com>
2017-11-12 21:15:08 -0200

    1.1
    3 - Faça check out Crie um novo atributo String A3 com seu set e get na classe A. Faça Check-in.
    
    4 - Faça check out Crie um novo método soma na classe A para retornar a soma dos atributos int e float. Faça Check-in.

---
+++ gerenciaDeSoftware.c
@@ -0,0 +1,29 @@
+/*
+Trabalho Prático de Gerência de Configurações
+Nome: Stella Azevedo Marques Matrícula: 201421234
+Ferramenta analisada GITHUB 
+*/
+
+
+/*
+Crie uma classe A com os atributos int A1 e float A2 e seus gets e sets. 
+Cadastre essa classe como um sistema a ser controlado 
+na ferramenta de Gerência de Configuração Essa será a versão 1.0
+ */
+
+int func_A()
+{
+  int A1 = 0;
+  int A2 = 0;
+  
+  return 0;	
+}
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
