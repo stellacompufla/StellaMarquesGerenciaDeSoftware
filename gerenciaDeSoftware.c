9b76f851765b552da372079f0b00bd89fd445942
Stella Marques <stellacompufla@gmail.com>
2017-11-12 21:26:37 -0200

    versao 1.1
    O sistema que está na base de dados será agora "fechado" e denominado de versão 1.1.

--- gerenciaDeSoftware.c
+++ gerenciaDeSoftware.c
@@ -9,13 +9,23 @@ Ferramenta analisada GITHUB
 Crie uma classe A com os atributos int A1 e float A2 e seus gets e sets. 
 Cadastre essa classe como um sistema a ser controlado 
 na ferramenta de Gerência de Configuração Essa será a versão 1.0
+
+3 - Faça check out Crie um novo atributo String A3 com seu set e get na classe A. Faça Check-in. 
+
+Faça check out Crie um novo método soma na classe A para retornar a soma dos atributos int e float. Faça Check-in.
  */
+ 
+int soma(int a, float b)
+{
+	return a+b;
+}
 
 int func_A()
 {
   int A1 = 0;
-  int A2 = 0;
-  
+  float A2 = 0;
+  char A3[10];
+  soma(A1,A2);
   return 0;	
 }
 
