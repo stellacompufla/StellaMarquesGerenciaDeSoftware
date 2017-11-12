4fdee4c87df1d01e7987640d8113f5ce7291135b
Stella Marques <stellacompufla@gmail.com>
2017-11-12 21:32:56 -0200

    versao 1.2
    Faça check out e insira novo atributo String Aramo4 com seu set e get na classe A. Essa alteração deverá ser um novo ramo de sua gerência de configuração. Faça Check-in. Crie a versão 1.2 do ramo.

--- gerenciaDeSoftware.c
+++ gerenciaDeSoftware.c
@@ -13,6 +13,10 @@ na ferramenta de Gerência de Configuração Essa será a versão 1.0
 3 - Faça check out Crie um novo atributo String A3 com seu set e get na classe A. Faça Check-in. 
 
 Faça check out Crie um novo método soma na classe A para retornar a soma dos atributos int e float. Faça Check-in.
+
+Faça check out e insira novo atributo String Aramo4 com seu set e get na classe A.
+Essa alteração deverá ser um novo ramo de sua gerência de configuração. Faça Check-in.
+Crie a versão 1.2 do ramo.
  */
  
 int soma(int a, float b)
@@ -25,6 +29,7 @@ int func_A()
   int A1 = 0;
   float A2 = 0;
   char A3[10];
+  char Aramo4[10];
   soma(A1,A2);
   return 0;	
 }
